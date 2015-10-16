3ce19f1d60e356365fb99b4af80af4975ac14e82
Brad Pousland <brad@pousland.com>
2015-10-16 14:19:46 -0500

    Fixed not having a context in async callback.

--- src/binding.cc
+++ src/binding.cc
@@ -490,6 +490,11 @@ void NodeDhclient::toV8_control(uv_async_t *handle, int status /*UNUSED*/) {
 
 	NodeDhclient *dhclient = (NodeDhclient *) handle->data;
 
+	auto isolate = Isolate::GetCurrent();
+	HandleScope scope(isolate);
+	auto context = isolate->GetCurrentContext();
+	auto global = context->Global();
+
 	const unsigned argc = 2;
 	Local<Value> argv[argc];
 
@@ -500,7 +505,8 @@ void NodeDhclient::toV8_control(uv_async_t *handle, int status /*UNUSED*/) {
 			{
 				// call leaseLog callback
 				if(!dhclient->leaseCallback->IsEmpty()) {
-					argv[0] = Nan::New(work->_backing,work->len).ToLocalChecked();
+					argv[0] = Nan::New<v8::String>(work->_backing,work->len).ToLocalChecked();
+					DBG_OUT("BOBBY!!!!!!!!!    ");
 					dhclient->leaseCallback->Call(Nan::GetCurrentContext()->Global(),1,argv);
 				} else {
 					ERROR_OUT("No leaseCallback set! dump: %s\n", work->_backing);
